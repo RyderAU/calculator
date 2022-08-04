a739048de8ec94a3fc9743244fd680d9cc85b240
ryder <ryder.jacka@gmail.com>
2022-08-05 02:41:38 +1000

    Basic operation functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hey");
+// create functions for operations
+// add
+const add = (a, b) => {
+  return a + b;
+};
+// subtract
+const subtract = (a, b) => {
+  return a - b;
+};
+// multiply
+const multiply = (a, b) => {
+  return a * b;
+};
+// divide
+const divide = (a, b) => {
+  return a / b;
+};
+// operate
+const operate = (operation, a, b) => {
+  if (operation == "+") {
+    return add(a, b);
+  } else if (operation == "-") {
+    return subtract(a, b);
+  } else if (operation == "*") {
+    return multiply(a, b);
+  } else if (operation == "/") {
+    return divide(a, b);
+  }
+};
